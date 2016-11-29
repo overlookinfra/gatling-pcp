4412041b64072f7b77230ee0fcbe7d462898fb9d
Michael Smith <michael.smith@puppet.com>
2016-11-29 14:51:10 -0800

    Add multiple clients via client_type

--- src/test/scala/wss.scala
+++ src/test/scala/wss.scala
@@ -5,17 +5,23 @@ import scala.concurrent.duration._
 class PCPConnections extends Simulation {
   val httpConf = http.wsBaseURL("wss://broker.example.com:8142")
 
+  var count = 0
+  def counter(): Int = { count += 1; count }
+  val feeder = Iterator.continually(Map("bytes" -> (pcp.associate_request("pcp://client01.example.com/"+counter()))))
+
   val scn = scenario("ConnectWebSocket")
+    .feed(feeder)
     .exec(ws("Connect WS").open("/pcp"))
-    .exec(ws("Association")
-      .sendBytes(pcp.associate_request("pcp://client01.example.com/test01"))
+    .exec(
+      ws("Association").sendBytes("${bytes}")
       // Checks don't work with binary messages. This may be fixed in Gatling 3.
       // We also plan to switch to text-based messages. Punt for now.
       //.check(wsAwait.within(10).until(1))
-      )
-    .exec(ws("Close WS").close)
+    )
+    // Leave all connections open until the end.
+    //.exec(ws("Close WS").close)
 
   setUp(
-    scn.inject(atOnceUsers(1))
+    scn.inject(constantUsersPerSec(150) during(20 seconds))
   ).protocols(httpConf)
 }
